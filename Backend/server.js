f787c6924ff560058897367d7b5bec4b7fb5350f
Er.Himanshu Tiwari <himanshu.tiwarii.dev@gmail.com>
2026-04-10 23:19:06 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -28,6 +28,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
